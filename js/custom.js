0961ccabd8fb6da0df22851c9724223d052248bf
Niels Slo <nielssloesen@hotmail.com>
2021-07-13 09:10:24 +0200

    Fixed issue on tablet for image slider

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,36 @@ $(document).ready(function () {
 		dots: true,
 		autoplay: true,
 		centerMode: true,
-		centerPadding: "calc(2.333em - 0.5em)",
-		slidesToShow: 1,
+		centerPadding: "calc(3.333em - 0.5em)",
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: "calc(2.333em - 0.5em)",
+					slidesToShow: 3,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: "calc(2.333em - 0.5em)",
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: "calc(2.333em - 0.5em)",
+					slidesToShow: 1,
+				},
+			},
+		],
 	});
 });
